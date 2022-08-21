645b304b99370895e954457f985655ca2d54a865
Aliherawi7 <Aliherawi7@gmail.com>
2022-08-21 13:18:16 -0700

    lombok annotations removed and contractor with getters and setters methods implemented

--- src/main/java/com/eshop/model/Product.java
+++ src/main/java/com/eshop/model/Product.java
@@ -1,16 +1,9 @@
 package com.eshop.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import javax.persistence.*;
 import java.time.LocalDate;
 
-
 @Entity
-@Data @NoArgsConstructor
-@AllArgsConstructor
 public class Product {
     @Id
     @SequenceGenerator(sequenceName = "product_sequence", name = "product_sequence", allocationSize = 1)
@@ -26,4 +19,99 @@ public class Product {
     private String description;
     private LocalDate productionDate;
     private String size;
+
+    public Product(){}
+
+    public Product(Integer id, String name, String color, String imgUrl, String brandName, String category, Double price, String description, LocalDate productionDate, String size) {
+        this.id = id;
+        this.name = name;
+        this.color = color;
+        this.imgUrl = imgUrl;
+        this.brandName = brandName;
+        this.category = category;
+        this.price = price;
+        this.description = description;
+        this.productionDate = productionDate;
+        this.size = size;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getBrandName() {
+        return brandName;
+    }
+
+    public void setBrandName(String brandName) {
+        this.brandName = brandName;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getProductionDate() {
+        return productionDate;
+    }
+
+    public void setProductionDate(LocalDate productionDate) {
+        this.productionDate = productionDate;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
 }
