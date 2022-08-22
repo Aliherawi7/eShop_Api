5d2f43bb464f7092843690a0744bd55c710d3ab4
Aliherawi7 <Aliherawi7@gmail.com>
2022-08-22 16:58:20 -0700

    UserRepository useful query method added

--- src/main/java/com/eshop/repository/UserRepository.java
+++ src/main/java/com/eshop/repository/UserRepository.java
@@ -1,4 +1,12 @@
 package com.eshop.repository;
 
-public interface UserRepository {
+import com.eshop.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Collection;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
+    User findByEmail(String email);
+    Collection<User> findByName(String name);
+
 }
