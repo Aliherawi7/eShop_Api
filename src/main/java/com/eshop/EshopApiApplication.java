d4d2e82792f803dd5fe69bb377085f2a7581bf44
Aliherawi7 <Aliherawi7@gmail.com>
2022-08-23 00:12:27 -0700

    BCryptPasswordEncoder bean created

--- src/main/java/com/eshop/EshopApiApplication.java
+++ src/main/java/com/eshop/EshopApiApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.time.LocalDate;
 
@@ -14,6 +15,11 @@ public class EshopApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EshopApiApplication.class, args);
+	}
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoderBean(){
+		return new BCryptPasswordEncoder();
 	}
 
 	@Bean
