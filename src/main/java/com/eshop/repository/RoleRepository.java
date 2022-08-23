be651bfa9992abd358190d811c0e7db3b2a52dcc
Aliherawi7 <Aliherawi7@gmail.com>
2022-08-22 17:09:13 -0700

    RoleRepository creatd and userful query methods added

---
+++ src/main/java/com/eshop/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.eshop.repository;
+
+import com.eshop.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Role findByName(String name);
+    boolean existsBy(String name);
+}
