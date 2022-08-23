f469bd93f2cc025dd6376ac2ea488cebac391521
Aliherawi7 <Aliherawi7@gmail.com>
2022-08-23 15:34:07 -0700

    CustomAuthenticationfilter added to config method

--- src/main/java/com/eshop/security/SecurityConfiguration.java
+++ src/main/java/com/eshop/security/SecurityConfiguration.java
@@ -1,7 +1,9 @@
 package com.eshop.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.eshop.filter.CustomAuthenticationFilter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,7 +15,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-    @Autowired
+
     private final UserDetailsService userDetailsService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
     public SecurityConfiguration(UserDetailsService userDetailsService, BCryptPasswordEncoder bCryptPasswordEncoder){
@@ -27,9 +29,19 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        CustomAuthenticationFilter authenticationFilter =
+                new CustomAuthenticationFilter(authenticationManagerBean());
+        authenticationFilter.setFilterProcessesUrl("/api/login");
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeHttpRequests().antMatchers("/api/login").permitAll();
         http.authorizeHttpRequests().anyRequest().permitAll();
+        http.addFilter(authenticationFilter);
 
+    }
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
