7fe8f393a13450e7374308427770cb0e2ecccfe0
Aliherawi7 <Aliherawi7@gmail.com>
2022-08-24 14:37:57 -0700

    Query method added for set the nummber of attempts that user failed where the user email is ...

--- src/main/java/com/eshop/repository/UserRepository.java
+++ src/main/java/com/eshop/repository/UserRepository.java
@@ -2,11 +2,16 @@ package com.eshop.repository;
 
 import com.eshop.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import java.util.Collection;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
     User findByEmail(String email);
     Collection<User> findByName(String name);
+
+    @Query("Update User u set u.failedAttempt = ?1 WHERE u.email = ?2")
+    void updateFailedAttempts(int failed, String email);
 
 }
