ec5a0d4458dbe97db1956f98a59ab27ac71bac7e
Aliherawi7 <Aliherawi7@gmail.com>
2022-08-24 00:25:15 -0700

    name changed to CustomAuthorizationFilter

--- src/main/java/com/eshop/filter/CustomAuthorizationFilter.java
+++ src/main/java/com/eshop/filter/CustomAuthorizationFilter.java
@@ -19,7 +19,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.*;
 
-public class CutomAuthorizationFilter extends OncePerRequestFilter {
+public class CustomAuthorizationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         //if the request is coming from login page so do nothing and pass the request to the next filter
