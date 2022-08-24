f14f0758d6f5be71ba429ba749abc973910c841b
Aliherawi7 <Aliherawi7@gmail.com>
2022-08-24 14:46:37 -0700

    code formatted

--- src/main/java/com/eshop/repository/UserRepository.java
+++ src/main/java/com/eshop/repository/UserRepository.java
@@ -3,7 +3,6 @@ package com.eshop.repository;
 import com.eshop.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
 import java.util.Collection;
 
 public interface UserRepository extends JpaRepository<User, Long> {
