956fd5521088612aaec1aff0a4e880163fcda69e
Aliherawi7 <Aliherawi7@gmail.com>
2022-08-31 14:15:24 -0700

    UserRepository name changed to UserAppRepsitory

--- src/main/java/com/eshop/repository/UserAppRepository.java
+++ src/main/java/com/eshop/repository/UserAppRepository.java
@@ -4,7 +4,7 @@ import com.eshop.model.UserApp;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Collection;
 
-public interface UserRepository extends JpaRepository<UserApp, Long> {
+public interface UserAppRepository extends JpaRepository<UserApp, Long> {
     boolean existsByEmail(String email);
     UserApp findByEmail(String email);
     Collection<UserApp> findByName(String name);
