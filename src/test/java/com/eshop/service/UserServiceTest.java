7c5d1619b804c16da622722e2774262e6c9f6237
Aliherawi7 <Aliherawi7@gmail.com>
2022-09-01 12:34:34 -0700

    all method tested except unlockWhenTimeExpired method

--- src/test/java/com/eshop/service/UserServiceTest.java
+++ src/test/java/com/eshop/service/UserServiceTest.java
@@ -1,23 +1,41 @@
 package com.eshop.service;
 
+import com.eshop.model.Role;
 import com.eshop.model.UserApp;
+import com.eshop.repository.RoleRepository;
 import com.eshop.repository.UserAppRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-@ExtendWith(MockitoAnnotations.class)
+@ExtendWith(MockitoExtension.class)
 class UserServiceTest {
-    @Mock
-    private UserAppRepository userAppRepository;
+
+    @Mock private UserAppRepository userAppRepository;
+    @Mock private RoleRepository roleRepository;
+    @Mock private BCryptPasswordEncoder bCryptPasswordEncoder;
     private UserService underTest;
+    private UserApp user;
+
 
     @BeforeEach
     void setUp() {
+        underTest = new UserService(userAppRepository, roleRepository, bCryptPasswordEncoder);
+        user = new UserApp();
+        user.setName("ali");
+        user.setLastName("herawi");
+        user.setEmail("aliherawi7@gmail.com");
+        user.setPassword("1234");
     }
 
     @Test
@@ -26,45 +44,181 @@ class UserServiceTest {
 
     @Test
     void addUser() {
+        //when
+        underTest.addUser(user);
+
+        //then
+        ArgumentCaptor<UserApp> userAppArgumentCaptor =
+                ArgumentCaptor.forClass(UserApp.class);
+        verify(userAppRepository).save(userAppArgumentCaptor.capture());
+        UserApp capturedUserApp = userAppArgumentCaptor.getValue();
+        assertEquals(capturedUserApp, user);
+
     }
 
     @Test
     void getUser() {
+        //when
+        String email = "aliherawi7@gmail.com";
+        underTest.getUser(email);
+        //then
+        verify(userAppRepository).findByEmail(email);
     }
 
     @Test
-    void addRoleToUser() {
+    //if the user and role exists
+    void addRoleToUserIfUserAndRoleExist() {
+        //given
+        Role role = new Role(1, "USER");
+        //when
+        when(userAppRepository.findByEmail(user.getEmail())).thenReturn(user);
+        when(roleRepository.findByName(role.getName())).thenReturn(role);
+
+        underTest.addRoleToUser(user.getEmail(), role.getName());
+        //then
+        verify(roleRepository).findByName(role.getName());
+        verify(userAppRepository).findByEmail(user.getEmail());
+        verify(userAppRepository).save(user);
+
+    }
+
+    @Test
+    //if the user not exists
+    void addRoleToUserIfUserAndRoleNotExist() {
+        //given
+        Role role = new Role(1, "USER");
+        //when
+        when(userAppRepository.findByEmail(user.getEmail())).thenReturn(null);
+        when(roleRepository.findByName(role.getName())).thenReturn(role);
+
+        underTest.addRoleToUser(user.getEmail(), role.getName());
+        //then
+        verify(roleRepository).findByName(role.getName());
+        verify(userAppRepository).findByEmail(user.getEmail());
+
     }
 
     @Test
     void getAllUsers() {
+        //when
+        underTest.getAllUsers();
+        //then
+        verify(userAppRepository).findAll();
     }
 
     @Test
-    void deleteUser() {
+    // if user is available and password is correct
+    void deleteUserIFUserIsAvailableAndPasswordIsCorrect() {
+        //given
+        String email = user.getEmail();
+        String password = user.getPassword();
+        // when
+        when(userAppRepository.findByEmail(email)).thenReturn(user);
+        when(bCryptPasswordEncoder.matches(password, user.getPassword())).thenReturn(true);
+        underTest.deleteUser(email,password);
+        //then
+        verify(userAppRepository).delete(user);
+    }
+    @Test
+    // if user is available and password is incorrect
+    void deleteUserIFUserIsAvailableAndPasswordIsIncorrect() {
+        //given
+        String email = user.getEmail();
+        String password = user.getPassword();
+        // when
+        when(userAppRepository.findByEmail(email)).thenReturn(user);
+        when(bCryptPasswordEncoder.matches(password, "fakePassword")).thenReturn(false);
+        underTest.deleteUser(email,"fakePassword");
+        //then
+        assertEquals(underTest.deleteUser(email, "fakePassword"),
+                new ResponseEntity<>("incorrect password!", HttpStatus.FORBIDDEN));
+    }
+    @Test
+        // if user is available and password is incorrect
+    void deleteUserIFUserIsNotAvailable() {
+        //given
+        String email = "fake-email";
+        String password = "fakePassword";
+        // when
+        when(userAppRepository.findByEmail(email)).thenReturn(null);
+        underTest.deleteUser(email,password);
+        //then
+        verify(userAppRepository).findByEmail(email);
+        assertEquals(underTest.deleteUser(email, password),
+                new ResponseEntity<>("user not found!", HttpStatus.NOT_FOUND));
     }
 
     @Test
+    // check if account is lock
     void isAccountLocked() {
+        // when
+        user.setAccountLocked(true);
+        // then
+        assertTrue(underTest.isAccountLocked(user));
+    }
+    @Test
+        // check if account is not lock
+    void isNotAccountLocked() {
+        // when
+        user.setAccountLocked(false);
+        // then
+        assertFalse(underTest.isAccountLocked(user));
     }
 
     @Test
     void increaseFailedAttempts() {
+        //when
+        int failedAttempt = user.getFailedAttempt();
+        when(userAppRepository.findByEmail(user.getEmail())).thenReturn(user);
+        underTest.increaseFailedAttempts(user);
+        // then
+        verify(userAppRepository).findByEmail(user.getEmail());
+        verify(userAppRepository).save(user);
+        assertNotEquals(failedAttempt, user.getFailedAttempt());
     }
 
     @Test
     void resetFailedAttempts() {
+        // given
+        user.setFailedAttempt(3);
+        //when
+        when(userAppRepository.findByEmail(user.getEmail())).thenReturn(user);
+        underTest.resetFailedAttempts(user.getEmail());
+
+        //then
+        verify(userAppRepository).findByEmail(user.getEmail());
+        verify(userAppRepository).save(user);
+        assertEquals(0, user.getFailedAttempt());
     }
 
     @Test
     void updateFailedAttempts() {
+
+        //when
+        when(userAppRepository.findByEmail(user.getEmail())).thenReturn(user);
+        underTest.updateFailedAttempts(3, user.getEmail());
+
+        //then
+        verify(userAppRepository).findByEmail(user.getEmail());
+        verify(userAppRepository).save(user);
+        assertEquals(3, user.getFailedAttempt());
     }
 
     @Test
     void lock() {
+        //when
+        underTest.lock(user);
+        //then
+        verify(userAppRepository).save(user);
+        assertTrue(user.isAccountLocked());
     }
 
     @Test
     void unlockWhenTimeExpired() {
+        //when
+        underTest.lock(user);
+        // then
+        verify(userAppRepository).save(user);
+        assertFalse(underTest.unlockWhenTimeExpired(user));
     }
 }
