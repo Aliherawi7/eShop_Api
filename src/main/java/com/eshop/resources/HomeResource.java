92f6f2ef486202eec66bdd22ccdd505c67311ce2
Aliherawi7 <Aliherawi7@gmail.com>
2022-09-07 23:50:26 -0700

    home controller created for testing ipFinder service

--- src/main/java/com/eshop/resources/HomeResource.java
+++ src/main/java/com/eshop/resources/HomeResource.java
@@ -1,4 +1,26 @@
 package com.eshop.resources;
 
+import com.eshop.service.IPFinderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
 public class HomeResource {
+
+    @Autowired
+    private IPFinderService ipFinderService;
+
+    @GetMapping("/")
+    public ResponseEntity<String> index(HttpServletRequest request){
+
+        String clientIP = ipFinderService.getClientIP(request);
+        return ResponseEntity.ok(clientIP);
+    }
+
 }
