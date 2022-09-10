8796c2c15d8b98a059b6fc1cf1d2e962c1f23c96
Aliherawi7 <Aliherawi7@gmail.com>
2022-09-09 22:03:47 -0700

    simple welcome message added to return as response

--- src/main/java/com/eshop/resources/HomeResource.java
+++ src/main/java/com/eshop/resources/HomeResource.java
@@ -1,26 +1,34 @@
 package com.eshop.resources;
 
 import com.eshop.service.IPFinderService;
+import com.eshop.service.ProductService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
-
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class HomeResource {
 
     @Autowired
     private IPFinderService ipFinderService;
+    @Autowired
+    private ProductService productService;
 
     @GetMapping("/")
     public ResponseEntity<String> index(HttpServletRequest request){
-
         String clientIP = ipFinderService.getClientIP(request);
-        return ResponseEntity.ok(clientIP);
+        ObjectMapper response = new ObjectMapper();
+        Map<String, String> responseValues = new HashMap<>();
+        responseValues.put("IP Address", clientIP);
+        responseValues.put("welcomeMessage", "Welcome to eshop store");
+
+        return new ResponseEntity(responseValues, HttpStatus.OK);
     }
 
 }
