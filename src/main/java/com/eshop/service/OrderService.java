f32828863e2a153dfc7cb7a678b8aab6f5279182
Aliherawi7 <Aliherawi7@gmail.com>
2022-09-10 16:05:41 -0700

    OrderRepository instance added, findById method implemented

--- src/main/java/com/eshop/service/OrderService.java
+++ src/main/java/com/eshop/service/OrderService.java
@@ -1,4 +1,27 @@
 package com.eshop.service;
 
+import com.eshop.model.Order;
+import com.eshop.repository.OrderRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
 public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public OrderService(OrderRepository orderRepository){
+        this.orderRepository = orderRepository;
+    }
+
+    // find order by id
+    public ResponseEntity<?> getOrder(Long id){
+        Order order = orderRepository.findById(id).orElse(null);
+        if(order != null){
+            return new ResponseEntity<>(order, HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>("order not found with id: "+id, HttpStatus.NOT_FOUND);
+        }
+    }
 }
