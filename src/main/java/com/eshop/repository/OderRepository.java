bfe9e59c12044883f5cc408efc5b121af21f7551
Aliherawi7 <Aliherawi7@gmail.com>
2022-09-10 12:49:01 -0700

    most used query methods added

--- src/main/java/com/eshop/repository/OderRepository.java
+++ src/main/java/com/eshop/repository/OderRepository.java
@@ -1,4 +1,20 @@
 package com.eshop.repository;
 
-public interface OderRepository {
+import com.eshop.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.Collection;
+
+public interface OderRepository extends JpaRepository<Order, Long> {
+    Collection<Order> findAllByUserIdAndProductId(Long userID, Long productId);
+    Collection<Order> findAllByUserId(Long userId);
+    Collection<Order> findAllByProductId(Long productId);
+    Collection<Order> findAllByOrderDate(LocalDate orderDate);
+    Collection<Order> findAllByOrderDateAndDelivered(LocalDate date, boolean delivered);
+    Collection<Order> findAllByDeliveredBefore(LocalDate date);
+    Collection<Order> findAllByDeliveredAfter(LocalDate date);
+    Collection<Order> findAllByDeliveredBetween(LocalDate startDate, LocalDate endDate);
+
+
 }
