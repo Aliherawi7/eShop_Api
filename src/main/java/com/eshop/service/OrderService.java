9eba864264051644b58edf575abcdefc28dd6523
Aliherawi7 <Aliherawi7@gmail.com>
2022-09-10 16:11:21 -0700

    findAllByUserIdAndProductId method implemented in orderService

--- src/main/java/com/eshop/service/OrderService.java
+++ src/main/java/com/eshop/service/OrderService.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
+
 @Service
 public class OrderService {
 
@@ -22,6 +24,17 @@ public class OrderService {
             return new ResponseEntity<>(order, HttpStatus.OK);
         }else{
             return new ResponseEntity<>("order not found with id: "+id, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    // find all orders with specific userId and ProductID
+    public ResponseEntity<?> findAllByUserIdAndProductId(Long userID, Long productId){
+        Collection<Order> orders = orderRepository.findAllByUserIdAndProductId(userID, productId);
+        if(orders.size() > 0){
+            return new ResponseEntity<>(orders, HttpStatus.OK);
+        }else{
+            return  new ResponseEntity<>
+                    ("No order available with user id: "+userID+ " and product id: "+productId, HttpStatus.NOT_FOUND);
         }
     }
 }
