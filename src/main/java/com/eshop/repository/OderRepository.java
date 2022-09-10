395bbd985b411bf6d6adce37e1cc327298637a5c
Aliherawi7 <Aliherawi7@gmail.com>
2022-09-10 15:53:06 -0700

    most used query method with date parameter added

--- src/main/java/com/eshop/repository/OderRepository.java
+++ src/main/java/com/eshop/repository/OderRepository.java
@@ -7,14 +7,38 @@ import java.time.LocalDate;
 import java.util.Collection;
 
 public interface OderRepository extends JpaRepository<Order, Long> {
-    Collection<Order> findAllByUserIdAndProductId(Long userID, Long productId);
-    Collection<Order> findAllByUserId(Long userId);
-    Collection<Order> findAllByProductId(Long productId);
-    Collection<Order> findAllByOrderDate(LocalDate orderDate);
-    Collection<Order> findAllByOrderDateAndDelivered(LocalDate date, boolean delivered);
-    Collection<Order> findAllByDeliveredBefore(LocalDate date);
-    Collection<Order> findAllByDeliveredAfter(LocalDate date);
-    Collection<Order> findAllByDeliveredBetween(LocalDate startDate, LocalDate endDate);
 
+    // find all orders with specific userId and ProductID
+    Collection<Order> findAllByUserIdAndProductId(Long userID, Long productId);
+
+    // find all orders with specific userId
+    Collection<Order> findAllByUserId(Long userId);
+
+    // find all orders with specific productID
+    Collection<Order> findAllByProductId(Long productId);
+
+    //find all order in a specific date
+    Collection<Order> findAllByOrderDate(LocalDate orderDate);
+
+    // find all delivered or not delivered orders in a specific date
+    Collection<Order> findAllByOrderDateAndDelivered(LocalDate date, boolean delivered);
+
+    // find all orders before the specific date
+    Collection<Order> findAllByOrderDateBefore(LocalDate date);
+
+    // find all orders after a specific date
+    Collection<Order> findAllByOrderDateAfter(LocalDate date);
+
+    // find all orders between two date
+    Collection<Order> findAllByOrderDateBetween(LocalDate startDate, LocalDate endDate);
+
+    // find all delivered order between two specific date
+    Collection<Order> findAllByOrderDateBetweenAndDelivered(LocalDate startDate, LocalDate endDate, boolean delivered);
+
+    // find All by userId productId and date
+    Collection<Order> findAllByUserIdAndProductIdAndOrderDateBetween(LocalDate start, LocalDate end);
+
+    // find all orders with specific userId and productId after the specific date
+    Collection<Order> findAllByUserIdAndProductIdAndOrderDateAfter(LocalDate start);
 
 }
