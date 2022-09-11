706114cda39f87c8366dad9518fbf0caaad620aa
Aliherawi7 <Aliherawi7@gmail.com>
2022-09-11 13:35:41 -0700

    addORder, deleteOrder, updateOrder and ... method implemented

--- src/main/java/com/eshop/resources/OrderResource.java
+++ src/main/java/com/eshop/resources/OrderResource.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/order")
 public class OrderResource {
@@ -25,6 +27,17 @@ public class OrderResource {
     @PostMapping("/save")
     public ResponseEntity<?> addOrder(@RequestBody Order order){
         return orderService.addOrder(order);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteOrder(@PathVariable Long id){
+        return orderService.deleteOrder(id);
+    }
+
+    // find all orders with specific userId and ProductID
+    @GetMapping(value = "/find", params = {"userId", "productId"})
+    public ResponseEntity<?> findAllByUserIdAndProductId(@RequestParam Map<String, Long> params){
+        return orderService.findAllByUserIdAndProductId(params.get("userId"), params.get("productId"));
     }
 
 }
