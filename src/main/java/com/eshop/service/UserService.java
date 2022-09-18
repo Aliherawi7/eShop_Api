cdbea19401c8725a82489d5771f11ab34c7bc9dd
Aliherawi7 <Aliherawi7@gmail.com>
2022-09-18 11:45:47 -0700

    codes reformatted

--- src/main/java/com/eshop/service/UserService.java
+++ src/main/java/com/eshop/service/UserService.java
@@ -13,9 +13,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
+
 /*
  * @Author: Aliherawi
  * @Github: Aliherawi7
@@ -31,7 +33,8 @@ public class UserService implements UserDetailsService {
     private final UserAppRepository userRepository;
     private final RoleRepository roleRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
-    public UserService(UserAppRepository userRepository, RoleRepository roleRepository, BCryptPasswordEncoder bCryptPasswordEncoder){
+
+    public UserService(UserAppRepository userRepository, RoleRepository roleRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userRepository = userRepository;
         this.roleRepository = roleRepository;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
@@ -41,7 +44,7 @@ public class UserService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         UserApp user = userRepository.findByEmail(email);
-        if(user == null){
+        if (user == null) {
             throw new UsernameNotFoundException("User not found!");
         }
         Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
@@ -52,12 +55,12 @@ public class UserService implements UserDetailsService {
     }
 
     //save user on database
-    public ResponseEntity<?> addUser(UserSignupDTO user){
+    public ResponseEntity<?> addUser(UserSignupDTO user) {
 
         boolean exist = userRepository.existsByEmail(user.getEmail().toLowerCase().trim());
-        if(exist){
+        if (exist) {
             return new ResponseEntity<String>("User already exist", HttpStatus.BAD_REQUEST);
-        }else{
+        } else {
             UserApp userApp = new UserApp();
             userApp.setEmail(user.getEmail().trim().toLowerCase());
             userApp.setName(user.getName().trim().toLowerCase());
@@ -71,87 +74,91 @@ public class UserService implements UserDetailsService {
     }
 
     // find user by email
-    public UserApp getUser(String email){
+    public UserApp getUser(String email) {
         return userRepository.findByEmail(email);
     }
 
     // add role to the user
-    public ResponseEntity<?> addRoleToUser(String email, String roleName){
+    public ResponseEntity<?> addRoleToUser(String email, String roleName) {
         UserApp user = userRepository.findByEmail(email);
         Role role = roleRepository.findByName(roleName);
-        if(user != null && role != null){
+        if (user != null && role != null) {
             user.addRole(role);
             userRepository.save(user);
-            return new ResponseEntity<>("role: "+roleName+ " added successfully to the user", HttpStatus.OK);
-        }else if(user == null && role ==null){
+            return new ResponseEntity<>("role: " + roleName + " added successfully to the user", HttpStatus.OK);
+        } else if (user == null && role == null) {
             return new ResponseEntity<>(
-                    "user with email: "+email+"" +
-                    " and role with name: "+
-                    roleName+" not found!", HttpStatus.NOT_FOUND);
-        }else if (user == null){
-            return new ResponseEntity<>("user with email: "+email+"", HttpStatus.NOT_FOUND);
-        }else{
-            return new ResponseEntity<>("role with name: "+ roleName+" not found!", HttpStatus.NOT_FOUND);
+                    "user with email: " + email + "" +
+                            " and role with name: " +
+                            roleName + " not found!", HttpStatus.NOT_FOUND);
+        } else if (user == null) {
+            return new ResponseEntity<>("user with email: " + email + "", HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<>("role with name: " + roleName + " not found!", HttpStatus.NOT_FOUND);
         }
     }
 
     // get all users from the database
-    public ResponseEntity<?> getAllUsers(){
-        Collection<UserApp>  users = userRepository.findAll();
-        if(users.size()>0){
+    public ResponseEntity<?> getAllUsers() {
+        Collection<UserApp> users = userRepository.findAll();
+        if (users.size() > 0) {
             return new ResponseEntity<>(users, HttpStatus.OK);
-        }else{
+        } else {
             return new ResponseEntity<>("no user is available!", HttpStatus.NOT_FOUND);
         }
     }
 
     // delete a user by email and password
-    public ResponseEntity<?> deleteUser(String email, String password){
+    public ResponseEntity<?> deleteUser(String email, String password) {
         email = email.trim().toLowerCase();
         UserApp user = userRepository.findByEmail(email);
         // if user is not available
-        if(user == null){
+        if (user == null) {
             return new ResponseEntity<>("user not found!", HttpStatus.NOT_FOUND);
         }
         boolean matchPassword = bCryptPasswordEncoder.matches(user.getPassword(), password);
 
-        if(matchPassword){
+        if (matchPassword) {
             userRepository.delete(user);
             return new ResponseEntity<>("user removed successfully", HttpStatus.OK);
-        }else{
-            return new ResponseEntity<>("incorrect password!",HttpStatus.FORBIDDEN);
+        } else {
+            return new ResponseEntity<>("incorrect password!", HttpStatus.FORBIDDEN);
         }
     }
 
     // check if account is locked
-    public boolean isAccountLocked(UserApp user)throws UsernameNotFoundException{
+    public boolean isAccountLocked(UserApp user) throws UsernameNotFoundException {
         return user.isAccountLocked();
     }
+
     // updates the number of failed attempts of a user. it also called each time the user fails to login
-    public void increaseFailedAttempts(UserApp user){
+    public void increaseFailedAttempts(UserApp user) {
         int newFailedAttempts = user.getFailedAttempt() + 1;
         updateFailedAttempts(newFailedAttempts, user.getEmail());
     }
+
     // sets number of failed attempts to zero. this method will called when user has logged in successfully.
-    public void resetFailedAttempts(String email){
+    public void resetFailedAttempts(String email) {
         updateFailedAttempts(0, email);
     }
 
-    public void updateFailedAttempts(int attempt, String email){
+    public void updateFailedAttempts(int attempt, String email) {
         UserApp user = userRepository.findByEmail(email);
         user.setFailedAttempt(attempt);
         userRepository.save(user);
     }
+
     // locks the user's account if the number of failed attempts reach the maximum allowed times.
-    public void lock(UserApp user){
+    public void lock(UserApp user) {
         user.setAccountLocked(true);
-        user.setLockTime(new Date(System.currentTimeMillis()+LOCK_TIME_DURATION));
+        user.setLockTime(new Date(System.currentTimeMillis() + LOCK_TIME_DURATION));
         userRepository.save(user);
     }
+
     // unlocks the user's account when lock duration expires, allowing the user to login as usual.
-    public boolean unlockWhenTimeExpired(UserApp user){
+    public boolean unlockWhenTimeExpired(UserApp user) {
         long lockTimeInMillis = user.getLockTime().getTime();
-        if(lockTimeInMillis < System.currentTimeMillis()){
+        if (lockTimeInMillis < System.currentTimeMillis()) {
             user.setAccountLocked(false);
             user.setLockTime(null);
             user.setFailedAttempt(0);
