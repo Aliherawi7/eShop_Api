bb3f2cb6799a488b5220e423648ff5cbe08f7b87
Aliherawi7 <Aliherawi7@gmail.com>
2022-09-18 11:46:43 -0700

    codes reformatted

--- src/main/java/com/eshop/service/OrderService.java
+++ src/main/java/com/eshop/service/OrderService.java
@@ -18,33 +18,33 @@ public class OrderService {
     private final IPFinderService ipFinderService;
 
     @Autowired
-    public OrderService(OrderRepository orderRepository, IPFinderService ipFinderService){
+    public OrderService(OrderRepository orderRepository, IPFinderService ipFinderService) {
         this.orderRepository = orderRepository;
         this.ipFinderService = ipFinderService;
     }
 
     // find order by id
-    public ResponseEntity<?> getOrder(Long id){
+    public ResponseEntity<?> getOrder(Long id) {
         OrderApp order = orderRepository.findById(id).orElse(null);
-        if(order != null){
+        if (order != null) {
             return new ResponseEntity<>(order, HttpStatus.OK);
-        }else{
-            return new ResponseEntity<>("order not found with id: "+id, HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<>("order not found with id: " + id, HttpStatus.NOT_FOUND);
         }
     }
 
     // find all orders
-    public ResponseEntity<?> getAllOrder(){
+    public ResponseEntity<?> getAllOrder() {
         Collection<OrderApp> orders = orderRepository.findAll();
-        if(orders.size() > 0){
+        if (orders.size() > 0) {
             return ResponseEntity.ok(orders);
-        }else {
+        } else {
             return new ResponseEntity<>(orders, HttpStatus.NOT_FOUND);
         }
     }
 
     // save order
-    public ResponseEntity<?> addOrder(OrderApp order , HttpServletRequest request){
+    public ResponseEntity<?> addOrder(OrderApp order, HttpServletRequest request) {
         // get user remoteAddress
         String userIPAddress = ipFinderService.getClientIP(request);
         order.setRemoteAddress(userIPAddress);
@@ -54,130 +54,130 @@ public class OrderService {
 
     // saves orders
 
-    public ResponseEntity<String> addOrders(Collection<OrderApp> orders, HttpServletRequest request){
+    public ResponseEntity<String> addOrders(Collection<OrderApp> orders, HttpServletRequest request) {
         orders.forEach(orderApp -> addOrder(orderApp, request));
         return ResponseEntity.ok("orders save successfully");
     }
 
     // update order
-    public ResponseEntity<?> updateOrder(OrderApp order){
+    public ResponseEntity<?> updateOrder(OrderApp order) {
         orderRepository.save(order);
         return new ResponseEntity<>("order save successfully.", HttpStatus.CREATED);
     }
 
     // delete an order
-    public ResponseEntity<?> deleteOrder(Long id){
+    public ResponseEntity<?> deleteOrder(Long id) {
         orderRepository.deleteById(id);
         return new ResponseEntity<>("order deleted successfully", HttpStatus.OK);
     }
 
     // find all orders with specific userId and ProductID
-    public ResponseEntity<?> findAllByUserIdAndProductId(Long userID, Long productId){
+    public ResponseEntity<?> findAllByUserIdAndProductId(Long userID, Long productId) {
         Collection<OrderApp> orders = orderRepository.findAllByUserIdAndProductId(userID, productId);
-        if(orders.size() > 0){
+        if (orders.size() > 0) {
             return new ResponseEntity<>(orders, HttpStatus.OK);
-        }else{
-            return  new ResponseEntity<>(orders, HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<>(orders, HttpStatus.NOT_FOUND);
         }
     }
 
 
     // find all orders with specific userId
-    public ResponseEntity<?> findAllByUserId(Long userId){
+    public ResponseEntity<?> findAllByUserId(Long userId) {
         Collection<OrderApp> orders = orderRepository.findAllByUserId(userId);
-        if(orders.size()>0){
+        if (orders.size() > 0) {
             return new ResponseEntity<>(orders, HttpStatus.OK);
-        }else{
+        } else {
             return new ResponseEntity<>(orders, HttpStatus.NOT_FOUND);
         }
     }
 
     // find all orders with specific productID
-    public ResponseEntity<?> findAllByProductId(Long productId){
+    public ResponseEntity<?> findAllByProductId(Long productId) {
         Collection<OrderApp> orders = orderRepository.findAllByProductId(productId);
-        if(orders.size()>0){
+        if (orders.size() > 0) {
             return new ResponseEntity<>(orders, HttpStatus.OK);
-        }else{
+        } else {
             return new ResponseEntity<>(orders, HttpStatus.NOT_FOUND);
         }
     }
 
     //find all order in a specific date
-    public ResponseEntity<?> findAllByOrderDate(LocalDate orderDate){
+    public ResponseEntity<?> findAllByOrderDate(LocalDate orderDate) {
         Collection<OrderApp> orders = orderRepository.findAllByOrderDate(orderDate);
-        if(orders.size()>0){
+        if (orders.size() > 0) {
             return new ResponseEntity<>(orders, HttpStatus.OK);
-        }else{
+        } else {
             return new ResponseEntity<>(orders, HttpStatus.NOT_FOUND);
         }
     }
 
     // find all delivered or not delivered orders in a specific date
-    public ResponseEntity<?> findAllByOrderDateAndDelivered(LocalDate date, boolean delivered){
+    public ResponseEntity<?> findAllByOrderDateAndDelivered(LocalDate date, boolean delivered) {
         Collection<OrderApp> orders = orderRepository.findAllByOrderDateAndDelivered(date, delivered);
-        if(orders.size() > 0){
-            return  new ResponseEntity<>(orders, HttpStatus.OK);
-        }else{
+        if (orders.size() > 0) {
+            return new ResponseEntity<>(orders, HttpStatus.OK);
+        } else {
             return new ResponseEntity<>(orders, HttpStatus.NOT_FOUND);
         }
     }
 
     // find all orders before the specific date
-    public ResponseEntity<?> findAllByOrderDateBefore(LocalDate date){
+    public ResponseEntity<?> findAllByOrderDateBefore(LocalDate date) {
         Collection<OrderApp> orders = orderRepository.findAllByOrderDateBefore(date);
-        if(orders.size() > 0){
+        if (orders.size() > 0) {
             return new ResponseEntity<>(orders, HttpStatus.OK);
-        }else{
+        } else {
             return new ResponseEntity<>(orders, HttpStatus.NOT_FOUND);
         }
     }
 
     // find all orders after a specific date
-    public ResponseEntity<?> findAllByOrderDateAfter(LocalDate date){
+    public ResponseEntity<?> findAllByOrderDateAfter(LocalDate date) {
         Collection<OrderApp> orders = orderRepository.findAllByOrderDateAfter(date);
-        if(orders.size() > 0){
+        if (orders.size() > 0) {
             return new ResponseEntity<>(orders, HttpStatus.OK);
-        }else{
-            return  new ResponseEntity<>(orders, HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<>(orders, HttpStatus.NOT_FOUND);
         }
     }
 
     // find all orders between two date
-    public ResponseEntity<?> findAllByOrderDateBetween(LocalDate startDate, LocalDate endDate){
+    public ResponseEntity<?> findAllByOrderDateBetween(LocalDate startDate, LocalDate endDate) {
         Collection<OrderApp> orders = orderRepository.findAllByOrderDateBetween(startDate, endDate);
-        if(orders.size() > 0){
+        if (orders.size() > 0) {
             return new ResponseEntity<>(orders, HttpStatus.OK);
-        }else{
+        } else {
             return new ResponseEntity<>(orders, HttpStatus.NOT_FOUND);
         }
     }
 
     // find all delivered order between two specific date
-    public ResponseEntity<?> findAllByOrderDateBetweenAndDelivered(LocalDate startDate, LocalDate endDate, boolean delivered){
+    public ResponseEntity<?> findAllByOrderDateBetweenAndDelivered(LocalDate startDate, LocalDate endDate, boolean delivered) {
         Collection<OrderApp> orders = orderRepository.findAllByOrderDateBetweenAndDelivered(startDate, endDate, delivered);
-        if(orders.size() > 0){
+        if (orders.size() > 0) {
             return new ResponseEntity<>(orders, HttpStatus.OK);
-        }else{
+        } else {
             return new ResponseEntity<>(orders, HttpStatus.NOT_FOUND);
         }
     }
 
     // find All by userId productId and date
-    public ResponseEntity<?> findAllByUserIdAndProductIdAndOrderDateBetween(Long userId, Long productId, LocalDate start, LocalDate end){
+    public ResponseEntity<?> findAllByUserIdAndProductIdAndOrderDateBetween(Long userId, Long productId, LocalDate start, LocalDate end) {
         Collection<OrderApp> orders = orderRepository.findAllByUserIdAndProductIdAndOrderDateBetween(userId, productId, start, end);
-        if(orders.size() > 0){
+        if (orders.size() > 0) {
             return new ResponseEntity<>(orders, HttpStatus.OK);
-        }else{
+        } else {
             return new ResponseEntity<>(orders, HttpStatus.NOT_FOUND);
         }
     }
 
     // find all orders with specific userId and productId after the specific date
-    public ResponseEntity<?> findAllByUserIdAndProductIdAndOrderDateAfter(Long userID, Long productId, LocalDate start){
+    public ResponseEntity<?> findAllByUserIdAndProductIdAndOrderDateAfter(Long userID, Long productId, LocalDate start) {
         Collection<OrderApp> orders = orderRepository.findAllByUserIdAndProductIdAndOrderDateAfter(userID, productId, start);
-        if(orders.size() >0){
+        if (orders.size() > 0) {
             return new ResponseEntity<>(orders, HttpStatus.OK);
-        }else{
+        } else {
             return new ResponseEntity<>(orders, HttpStatus.NOT_FOUND);
         }
     }
--- src/main/java/com/eshop/service/ProductService.java
+++ src/main/java/com/eshop/service/ProductService.java
@@ -5,112 +5,122 @@ import com.eshop.model.Product;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
 import java.util.Collection;
 import java.util.Optional;
 
 @Service
 public class ProductService {
     private final ProductRepository productRepository;
-    public ProductService(ProductRepository productRepository){
+
+    public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
     //find product by id
 
-    public ResponseEntity<?> getProductById(int id){
+    public ResponseEntity<?> getProductById(int id) {
         Optional<Product> product = productRepository.findById(id);
-        if(product.isPresent()){
+        if (product.isPresent()) {
             return new ResponseEntity<>(product.get(), HttpStatus.OK);
 
-        }else{
-            return new ResponseEntity<>("product with id: "+id+ " not found!", HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<>("product with id: " + id + " not found!", HttpStatus.NOT_FOUND);
         }
     }
 
     //find all product in the database by name
-    public Product getProductByName(String name){
+    public Product getProductByName(String name) {
         return productRepository.findByName(name);
     }
+
     // save the product to the database
-    public ResponseEntity<String> addProduct(Product product){
+    public ResponseEntity<String> addProduct(Product product) {
         productRepository.save(product);
         return new ResponseEntity<>("Successfully saved!", HttpStatus.CREATED);
     }
+
     // update the existed product
-    public ResponseEntity<String> updateProduct(Product product){
+    public ResponseEntity<String> updateProduct(Product product) {
         boolean isExist = productRepository.existsById(product.getId());
-        if(isExist){
+        if (isExist) {
             productRepository.save(product);
-            return new ResponseEntity<String>("Successfully updated! !",HttpStatus.CREATED);
-        }else{
-            return new ResponseEntity<>("not Found such product with id: "+product.getId(), HttpStatus.NOT_FOUND);
+            return new ResponseEntity<String>("Successfully updated! !", HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity<>("not Found such product with id: " + product.getId(), HttpStatus.NOT_FOUND);
         }
     }
 
     //find all product in the database
-    public Collection<Product> getAllProducts(){
+    public Collection<Product> getAllProducts() {
         return productRepository.findAll();
     }
 
     // find all product by brand name
-    public Collection<Product> getAllByBrandName(String brandName){
+    public Collection<Product> getAllByBrandName(String brandName) {
         return productRepository.findAllByBrandName(brandName);
     }
+
     // find all product by category
-    public Collection<Product> getAllByCategory(String category){
+    public Collection<Product> getAllByCategory(String category) {
         return productRepository.findAllByCategory(category);
     }
 
     // find product by category and brand name and price greater than or equal to the given price
-    public Collection<Product> getAllByCategoryAndBrandNameAndPriceGreaterThanEqual(String category, String brandName, Double price){
+    public Collection<Product> getAllByCategoryAndBrandNameAndPriceGreaterThanEqual(String category, String brandName, Double price) {
         return productRepository.findAllByCategoryAndBrandNameAndPriceGreaterThan(category, brandName, price);
     }
+
     // find product by category and brand name and price less than or equal to the given price
-    public Collection<Product> getAllByCategoryAndBrandNameAndPriceLessThanEqual(String category, String brandName, Double price){
+    public Collection<Product> getAllByCategoryAndBrandNameAndPriceLessThanEqual(String category, String brandName, Double price) {
         return productRepository.findAllByCategoryAndBrandNameAndPriceLessThan(category, brandName, price);
     }
 
     // find all product by category and price greater or equal to the given price
-    public Collection<Product> getAllByPriceGreaterThanEqual(Double price){
+    public Collection<Product> getAllByPriceGreaterThanEqual(Double price) {
         return productRepository.findAllByPriceGreaterThanEqual(price);
     }
+
     //find all product by price less than or equal to the given price
-    public Collection<Product> getAllByPriceLessThanEqual(Double price){
+    public Collection<Product> getAllByPriceLessThanEqual(Double price) {
         return productRepository.findAllByPriceLessThanEqual(price);
     }
 
     // find all product by category and price less than or equal to the given price
-    public Collection<Product> getAllByCategoryAndPriceLessThanEqual(String category, Double price){
-        return productRepository.findAllByCategoryAndPriceLessThanEqual(category,price);
+    public Collection<Product> getAllByCategoryAndPriceLessThanEqual(String category, Double price) {
+        return productRepository.findAllByCategoryAndPriceLessThanEqual(category, price);
     }
+
     // find all product by category and price greater than or equal to the given price
-    public Collection<Product> getAllByCategoryAndPriceGreaterThanEqual(String category, Double price){
-        return productRepository.findAllByCategoryAndPriceGreaterThanEqual(category,price);
+    public Collection<Product> getAllByCategoryAndPriceGreaterThanEqual(String category, Double price) {
+        return productRepository.findAllByCategoryAndPriceGreaterThanEqual(category, price);
     }
 
     // find all product by brand name and price less than or equal to the given price
-     public Collection<Product> getAllByBrandNameAndPriceLessThanEqual(String brandName, Double price){
+    public Collection<Product> getAllByBrandNameAndPriceLessThanEqual(String brandName, Double price) {
         return productRepository.findAllByBrandNameAndPriceLessThanEqual(brandName, price);
-     }
-    // find all product by brand name and price greater than or equal to the given price
-     public Collection<Product> getAllByBrandNameAndPriceGreaterThanEqual(String brandName, Double price){
-        return productRepository.findAllByBrandNameAndPriceGreaterThanEqual(brandName, price);
-     }
+    }
 
-     // danger area
+    // find all product by brand name and price greater than or equal to the given price
+    public Collection<Product> getAllByBrandNameAndPriceGreaterThanEqual(String brandName, Double price) {
+        return productRepository.findAllByBrandNameAndPriceGreaterThanEqual(brandName, price);
+    }
+
+    // danger area
     //removed product
-    public ResponseEntity<String> deleteProductById(Integer id){
+    public ResponseEntity<String> deleteProductById(Integer id) {
         boolean isExist = productRepository.existsById(id);
         productRepository.deleteById(id);
-        if(isExist){
-            return new ResponseEntity<>("product with id: "+id +" successfully removed", HttpStatus.OK);
-        }else{
-            return new ResponseEntity<>("not found any product with id: "+id, HttpStatus.NOT_FOUND);
+        if (isExist) {
+            return new ResponseEntity<>("product with id: " + id + " successfully removed", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("not found any product with id: " + id, HttpStatus.NOT_FOUND);
         }
 
     }
+
     // remove all all product in the database
-    public ResponseEntity<String> deleteAllProducts(){
+    public ResponseEntity<String> deleteAllProducts() {
         productRepository.deleteAll();
         return new ResponseEntity<>("all data removed successfully!", HttpStatus.ACCEPTED);
     }
