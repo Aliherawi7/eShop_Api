205338e23730cbfb1859d50f0526a1b868f78168
Aliherawi7 <Aliherawi7@gmail.com>
2022-09-18 12:00:45 -0700

    properties added

--- src/main/java/com/eshop/dto/AddRoleToUserDTO.java
+++ src/main/java/com/eshop/dto/AddRoleToUserDTO.java
@@ -1,4 +1,24 @@
 package com.eshop.dto;
 
-public class addRoleToUserDTO {
+public class AddRoleToUserDTO {
+    private String userEmail;
+    private String RoleName;
+
+    public AddRoleToUserDTO(){}
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getRoleName() {
+        return RoleName;
+    }
+
+    public void setRoleName(String roleName) {
+        RoleName = roleName;
+    }
 }
