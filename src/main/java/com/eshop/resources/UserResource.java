583f0edada626bda903660a172d1d4484143bd5e
Aliherawi7 <Aliherawi7@gmail.com>
2022-09-18 12:02:03 -0700

    /signup route added for getting user information while signning up

--- src/main/java/com/eshop/resources/UserResource.java
+++ src/main/java/com/eshop/resources/UserResource.java
@@ -1,6 +1,9 @@
 package com.eshop.resources;
 
+import com.eshop.dto.AddRoleToUserDTO;
 import com.eshop.dto.EmailAndPasswordDTO;
+import com.eshop.dto.UserSignupDTO;
+import com.eshop.model.Role;
 import com.eshop.model.UserApp;
 import com.eshop.service.UserService;
 import org.springframework.http.HttpStatus;
@@ -12,35 +15,43 @@ import org.springframework.web.bind.annotation.*;
 public class UserResource {
 
     private final UserService userService;
-    public UserResource(UserService userService){
+
+    public UserResource(UserService userService) {
         this.userService = userService;
     }
 
     @GetMapping
-    public ResponseEntity<?> getAllUsers(){
+    public ResponseEntity<?> getAllUsers() {
         return userService.getAllUsers();
     }
 
+    @PostMapping("/signup")
+    public ResponseEntity<?> addUser(UserSignupDTO userInfo){
+        return userService.addUser(userInfo);
+    }
+
     @GetMapping("/user/{email}")
-    public ResponseEntity<?> getUser(@PathVariable String email){
+    public ResponseEntity<?> getUser(@PathVariable String email) {
         email = email.trim().toLowerCase();
         UserApp user = userService.getUser(email);
-        if(user != null){
+        if (user != null) {
             return new ResponseEntity<>(user, HttpStatus.OK);
-        }else{
-            return  new ResponseEntity<>("User with email: "+email+" not found!", HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<>("User with email: " + email + " not found!", HttpStatus.NOT_FOUND);
         }
     }
 
     @PostMapping("/delete")
-    public ResponseEntity<?> deleteUser(@RequestBody EmailAndPasswordDTO emailAndPassword){
+    public ResponseEntity<?> deleteUser(@RequestBody EmailAndPasswordDTO emailAndPassword) {
         return userService.deleteUser(emailAndPassword.getEmail(), emailAndPassword.getPassword());
     }
 
-   /* @PostMapping("/changepassword")
-    public ResponseEntity<?> changePassword(){
+   @PostMapping("/addRoleToUser")
+   public ResponseEntity<?> addRoleToUser(AddRoleToUserDTO emailAndPassword){
+        return userService
+                .addRoleToUser(emailAndPassword.getUserEmail(), emailAndPassword.getRoleName());
+   }
 
-    }*/
 
 
 }
