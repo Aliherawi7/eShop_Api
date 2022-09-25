d51c6825ffacac99490622c126357bbe5d592aa8
Aliherawi7 <Aliherawi7@gmail.com>
2022-09-24 17:27:04 -0700

    SingupInformationDTO created for returning user information plus jwt token to the frontend

--- src/main/java/com/eshop/dto/SignupInformationDTO.java
+++ src/main/java/com/eshop/dto/SignupInformationDTO.java
@@ -1,4 +1,29 @@
 package com.eshop.dto;
 
 public class SignupInformationDTO {
+    private String accessToken;
+    private UserInformationDTO userInformationDTO;
+
+    public SignupInformationDTO() { }
+
+    public SignupInformationDTO(String accessToken, UserInformationDTO userInformationDTO) {
+        this.accessToken = accessToken;
+        this.userInformationDTO = userInformationDTO;
+    }
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public UserInformationDTO getUserInformationDTO() {
+        return userInformationDTO;
+    }
+
+    public void setUserInformationDTO(UserInformationDTO userInformationDTO) {
+        this.userInformationDTO = userInformationDTO;
+    }
 }
+
