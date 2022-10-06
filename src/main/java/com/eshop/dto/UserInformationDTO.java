27c5e16a7321293f9720b4e33a49456a5fac739b
Aliherawi7 <Aliherawi7@gmail.com>
2022-10-06 11:32:45 -0700

    id, location, totalOrders, totalSpending and isActive properties addedto the userIformationDTO

--- src/main/java/com/eshop/dto/UserInformationDTO.java
+++ src/main/java/com/eshop/dto/UserInformationDTO.java
@@ -3,21 +3,39 @@ package com.eshop.dto;
 import java.util.List;
 
 public class UserInformationDTO {
+    private Long id;
     private String name;
     private String lastName;
     private byte[] image;
     private String email;
     private List<String> roles;
+    private String location;
+    private int totalOrders;
+    private double totalSpending;
+    private boolean isActive;
 
     public UserInformationDTO() {
     }
 
-    public UserInformationDTO(String name, String lastName, byte[] image, String email, List<String> roles) {
+    public UserInformationDTO(Long id, String name, String lastName, byte[] image, String email, List<String> roles, String location, int totalOrders, double totalSpending, boolean isActive) {
+        this.id = id;
         this.name = name;
         this.lastName = lastName;
         this.image = image;
         this.email = email;
         this.roles = roles;
+        this.location = location;
+        this.totalOrders = totalOrders;
+        this.totalSpending = totalSpending;
+        this.isActive = isActive;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -58,5 +76,37 @@ public class UserInformationDTO {
 
     public void setRoles(List<String> roles) {
         this.roles = roles;
+    }
+
+    public int getTotalOrders() {
+        return totalOrders;
+    }
+
+    public void setTotalOrders(int totalOrders) {
+        this.totalOrders = totalOrders;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        location = location;
+    }
+
+    public double getTotalSpending() {
+        return totalSpending;
+    }
+
+    public void setTotalSpending(double totalSpending) {
+        this.totalSpending = totalSpending;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean isActive) {
+        this.isActive = isActive;
     }
 }
