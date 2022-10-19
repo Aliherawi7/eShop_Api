751dc2777135c2a24c4f5d5a463c00864de40a24
Aliherawi7 <Aliherawi7@gmail.com>
2022-10-19 22:15:40 +0430

    Brand Service class implemented

--- src/main/java/com/eshop/service/BrandService.java
+++ src/main/java/com/eshop/service/BrandService.java
@@ -1,4 +1,38 @@
 package com.eshop.service;
 
+import com.eshop.model.Brand;
+import com.eshop.repository.BrandRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Optional;
+
+@Service
 public class BrandService {
+
+    private final BrandRepository brandRepository;
+
+    public BrandService(BrandRepository brandRepository){
+        this.brandRepository = brandRepository;
+    }
+
+    public Collection<Brand> getAllBrands(){
+        return brandRepository.findAll();
+    }
+
+    public Brand getBrand(int id){
+        Optional<Brand> optionalBrand = brandRepository.findById(id);
+        return optionalBrand.orElse(null);
+    }
+
+    public Brand getBrandByName(String name){
+        Optional<Brand> optionalBrand = brandRepository.findByName(name);
+        return optionalBrand.orElse(null);
+    }
+    public void addBrand(Brand brand){
+        brandRepository.save(brand);
+    }
+    public void updateBrand(Brand brand){
+        brandRepository.save(brand);
+    }
 }
