c01bdfa20454d77f6d0b7961204360db2a399dc8
Aliherawi7 <Aliherawi7@gmail.com>
2022-10-19 22:08:13 +0430

    all properties and behavior added

--- src/main/java/com/eshop/model/Brand.java
+++ src/main/java/com/eshop/model/Brand.java
@@ -1,4 +1,59 @@
 package com.eshop.model;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
 public class Brand {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String name;
+    @Lob
+    private byte[] logo;
+    private LocalDateTime addDate;
+
+    public Brand() {
+        addDate = LocalDateTime.now();
+    }
+
+    public Brand(Integer id, String name, byte[] logo) {
+        this.id = id;
+        this.name = name;
+        this.logo = logo;
+        this.addDate = LocalDateTime.now();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public byte[] getLogo() {
+        return logo;
+    }
+
+    public void setLogo(byte[] logo) {
+        this.logo = logo;
+    }
+
+    public LocalDateTime getAddDate() {
+        return addDate;
+    }
+
+    public void setAddDate(LocalDateTime addDate) {
+        this.addDate = addDate;
+    }
 }
