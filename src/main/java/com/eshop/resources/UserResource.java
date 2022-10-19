da3c2f1f5eb98b44a6d620fe1ec078ce631dcb8b
Aliherawi7 <Aliherawi7@gmail.com>
2022-10-19 22:14:06 +0430

    addUser method updated. User image added as parameter

--- src/main/java/com/eshop/resources/UserResource.java
+++ src/main/java/com/eshop/resources/UserResource.java
@@ -2,15 +2,22 @@ package com.eshop.resources;
 
 import com.eshop.dto.AddRoleToUserDTO;
 import com.eshop.dto.EmailAndPasswordDTO;
+import com.eshop.dto.UserInformationDTO;
 import com.eshop.dto.UserSignupDTO;
 import com.eshop.model.Role;
 import com.eshop.model.UserApp;
+import com.eshop.service.IPFinderService;
 import com.eshop.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.websocket.server.PathParam;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/users")
@@ -28,7 +35,17 @@ public class UserResource {
     }
 
     @PostMapping("/signup")
-    public ResponseEntity<?> addUser(@ RequestBody UserSignupDTO userInfo, HttpServletRequest request){
+    public ResponseEntity<?> addUser(@RequestParam("image")MultipartFile file, @RequestParam Map<String, String> params, HttpServletRequest request) throws IOException {
+        UserSignupDTO userInfo = new UserSignupDTO();
+        userInfo.setName(params.get("name"));
+        userInfo.setLastName(params.get("lastName"));
+        userInfo.setEmail(params.get("email"));
+        userInfo.setPassword(params.get("password"));
+        userInfo.setImage(file.getBytes());
+        userInfo.setDob(LocalDate.parse(params.get("dob")));
+        String ipAddress = IPFinderService.getClientIP(request);
+        String countryName = IPFinderService.getCountryName(ipAddress);
+        userInfo.setLocation(countryName);
         return userService.addUser(userInfo);
     }
 
