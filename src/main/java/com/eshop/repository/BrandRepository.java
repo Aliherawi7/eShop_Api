e979a7575d807d46eea0043f97ef0cb62c178543
Aliherawi7 <Aliherawi7@gmail.com>
2022-10-19 22:11:12 +0430

    Brand resouce and Brand repository implementedc

--- src/main/java/com/eshop/repository/BrandRepository.java
+++ src/main/java/com/eshop/repository/BrandRepository.java
@@ -1,4 +1,10 @@
 package com.eshop.repository;
 
-public interface BrandRepository {
+import com.eshop.model.Brand;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface BrandRepository extends JpaRepository<Brand, Integer> {
+    Optional<Brand> findByName(String name);
 }
