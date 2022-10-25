82188ac787a7f85bd337738e7a1a54fd45d37590
Aliherawi7 <Aliherawi7@gmail.com>
2022-10-25 16:01:57 +0430

    resource for statistics model added

--- src/main/java/com/eshop/resources/StatisticsResources.java
+++ src/main/java/com/eshop/resources/StatisticsResources.java
@@ -1,4 +1,24 @@
 package com.eshop.resources;
 
+import com.eshop.dto.SummeryDTO;
+import com.eshop.service.StatisticsService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/statistics")
 public class StatisticsResources {
+    private final StatisticsService statisticsService;
+
+
+    public StatisticsResources(StatisticsService statisticsService){
+        this.statisticsService = statisticsService;
+    }
+
+    @GetMapping("/models")
+    public ResponseEntity<SummeryDTO> getSummery(){
+        return ResponseEntity.ok().body(statisticsService.getModelSummery());
+    }
 }
