ff78ac07ba0a8a98f80a368fbebca3713bd97b12
Aliherawi7 <Aliherawi7@gmail.com>
2022-10-25 16:08:15 +0430

    Neccessary properties and methods added

--- src/main/java/com/eshop/service/StatisticsService.java
+++ src/main/java/com/eshop/service/StatisticsService.java
@@ -1,4 +1,31 @@
 package com.eshop.service;
 
+import com.eshop.dto.SummeryDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+
+@Service
 public class StatisticsService {
+    private final OrderService orderService;
+    private final  ProductService productService;
+    private final UserService userService;
+    private final BrandService brandService;
+
+    public StatisticsService(ProductService productService, OrderService orderService,
+                             UserService userService, BrandService brandService){
+        this.orderService = orderService;
+        this.productService = productService;
+        this.userService = userService;
+        this.brandService = brandService;
+    }
+
+    public SummeryDTO getModelSummery(){
+        int products = productService.getAllProducts().size();
+        int orders = orderService.getAllOrderSize();
+        int users = userService.getAllUserSize();
+        int categories = 5;
+        int brands = brandService.getAllBrandSize();
+        return new SummeryDTO(products, orders, categories, users, brands);
+    }
 }
