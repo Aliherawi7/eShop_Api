28e0066df684759715c2550fb79a7f29b00e27dd
Aliherawi7 <Aliherawi7@gmail.com>
2022-10-28 22:42:12 +0430

    getSummaryByMonth method created for getting models summary

--- src/main/java/com/eshop/resources/StatisticsResources.java
+++ src/main/java/com/eshop/resources/StatisticsResources.java
@@ -1,11 +1,22 @@
 package com.eshop.resources;
 
+import com.eshop.dto.MonthlyDataDTO;
 import com.eshop.dto.SummeryDTO;
+import com.eshop.model.OrderApp;
 import com.eshop.service.StatisticsService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
 
 @RestController
 @RequestMapping("/api/statistics")
@@ -20,5 +31,26 @@ public class StatisticsResources {
     @GetMapping("/models")
     public ResponseEntity<SummeryDTO> getSummery(){
         return ResponseEntity.ok().body(statisticsService.getModelSummery());
+    }
+
+    @GetMapping(value = "/summaryByMonth", params = {"model"})
+    public ResponseEntity<?> getSummaryByMonth(@RequestParam String model){
+        model = model.trim().toLowerCase();
+        MonthlyDataDTO monthlyDataDTO = null;
+
+        switch (model){
+            case "orders":{
+                monthlyDataDTO = statisticsService.totalOrdersMonthly();
+                break;
+            }
+            case "users":{
+
+                break;
+            }
+            default:{
+                return new ResponseEntity<>("not content!", HttpStatus.NO_CONTENT);
+            }
+        }
+        return ResponseEntity.ok().body(monthlyDataDTO);
     }
 }
