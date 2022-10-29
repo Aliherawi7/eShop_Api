0101f7e3bf11516fc8a322a58aad4ce480786d52
Aliherawi7 <Aliherawi7@gmail.com>
2022-10-29 11:50:23 +0430

    totalJoinedUserMonthly and totalAddedProductsMonthly methods implmented. utility methods added for clean code of the other methods

--- src/main/java/com/eshop/service/StatisticsService.java
+++ src/main/java/com/eshop/service/StatisticsService.java
@@ -4,6 +4,7 @@ import com.eshop.dto.MonthlyDataDTO;
 import com.eshop.dto.SummeryDTO;
 import com.eshop.model.OrderApp;
 import com.eshop.model.Product;
+import com.eshop.model.UserApp;
 import com.eshop.repository.BrandRepository;
 import com.eshop.repository.OrderRepository;
 import com.eshop.repository.ProductRepository;
@@ -22,6 +23,8 @@ public class StatisticsService {
     private final UserAppRepository userAppRepository;
     private final BrandRepository brandRepository;
     final String[] monthsArray = {"Jan","Feb","Mar","Apr","May","Jun","July","Aug","Sep","Oct","Nov","Dec"};
+
+
 
     public StatisticsService(ProductRepository productRepository, OrderRepository orderRepository,
                              UserAppRepository userAppRepository, BrandRepository brandRepository){
@@ -48,13 +51,17 @@ public class StatisticsService {
         return productRepository.findAll();
     }
 
+    public Collection<UserApp> getAllUsers(){
+        return userAppRepository.findAll();
+    }
+
     public MonthlyDataDTO totalOrdersMonthly(){
         ArrayList<String> months = new ArrayList<>();
         ArrayList<Integer> data = new ArrayList<>();
         int year = LocalDate.now().getYear(); // current year
 
-        // find the current month name and add it to the month list
-        String lastMonth = LocalDate.now().getMonth().toString().toLowerCase().substring(0, 3);
+        // current month
+        String lastMonth = findCurrentMonth();
         months.add(lastMonth);
         // calculate the total orders of the current month
         int currentYear = year;
@@ -67,13 +74,7 @@ public class StatisticsService {
         data.add(dataOfLastMonth);
 
         // find the current month index in the monthsArray
-        int index = -1;
-        for(int i =0; i<monthsArray.length; i++){
-            if(monthsArray[i].toLowerCase().startsWith(lastMonth.toLowerCase())){
-                index = i;
-                break;
-            }
-        }
+        int index = findIndexOfCurrentMonth(lastMonth);
 
         // get the name of the last 11 months with their data
         for(int i = 1; i <= 11; i++){
@@ -98,4 +99,108 @@ public class StatisticsService {
         return new MonthlyDataDTO(months, data);
     }
 
+    public MonthlyDataDTO totalJoinedUserMonthly(){
+        ArrayList<String> months = new ArrayList<>();
+        ArrayList<Integer> data = new ArrayList<>();
+        int year = LocalDate.now().getYear(); // current year
+
+        // current month
+        String lastMonth = findCurrentMonth();
+        months.add(lastMonth);
+
+        // calculate the total joined users of the current month
+        int currentYear = year;
+        int dataOfLastMonth = (int) getAllUsers().stream().filter(item ->
+                item.getJoinedDate().getYear() == currentYear && item.getJoinedDate().getMonth().toString().toLowerCase().startsWith(lastMonth)).count();
+
+        // add the total orders of the current month into data array
+        data.add(dataOfLastMonth);
+
+        // find the current month index in the monthsArray
+        int index = findIndexOfCurrentMonth(lastMonth);
+
+        // get the name of the last 11 months with their data
+        for(int i = 1; i <= 11; i++){
+            index--;
+            if(index < 0){
+                index = 11;
+                year--;
+            }
+            int tempIndex = index;
+            int tempCurrentYear = year;
+            int totalOrders = (int) getAllUsers().stream().filter(item -> item.getJoinedDate().getYear() == tempCurrentYear &&
+                    item.getJoinedDate().getMonth().toString()
+                            .toLowerCase().startsWith(monthsArray[tempIndex].toLowerCase())).count();
+            // add the total orders of the month to data array
+            data.add(totalOrders);
+            // add the month name to the month array
+            months.add(monthsArray[index].substring(0, 3));
+
+        }
+        return new MonthlyDataDTO(months, data);
+
+    }
+
+    public MonthlyDataDTO totalAddedProductsMonthly(){
+        ArrayList<String> months = new ArrayList<>();
+        ArrayList<Integer> data = new ArrayList<>();
+
+        // current month
+        String lastMonth = findCurrentMonth();
+        // current year
+        int year = LocalDate.now().getYear();
+        months.add(lastMonth);
+
+        // calculate the total joined users of the current month
+        int currentYear = year;
+        int dataOfLastMonth = (int) getAllProducts().stream().filter(item ->
+                item.getAddedDate().getYear() == currentYear && item.getAddedDate().getMonth()
+                        .toString().toLowerCase().startsWith(lastMonth)).count();
+
+        // add the total added products of the current month into data array
+        data.add(dataOfLastMonth);
+
+        // find the current month index in the monthsArray
+        int index = findIndexOfCurrentMonth(lastMonth);
+
+        // get the name of the last 11 months with their data
+        for(int i = 1; i <= 11; i++){
+            index--;
+            if(index < 0){
+                index = 11;
+                year--;
+            }
+            int tempIndex = index;
+            int tempCurrentYear = year;
+            int totalAddedProducts = (int) getAllProducts().stream().filter(item -> item.getAddedDate().getYear() == tempCurrentYear &&
+                    item.getAddedDate().getMonth().toString()
+                            .toLowerCase().startsWith(monthsArray[tempIndex].toLowerCase())).count();
+            // add the total added products of the month to data array
+            data.add(totalAddedProducts);
+            // add the month name to the month array
+            months.add(monthsArray[index].substring(0, 3));
+        }
+        return new MonthlyDataDTO(months, data);
+    }
+
+
+    /**********************************
+    *          Utility methods        *
+    ***********************************/
+
+    public int findIndexOfCurrentMonth(String month){
+        int index = -1;
+        for(int i =0; i<monthsArray.length; i++){
+            if(monthsArray[i].toLowerCase().startsWith(month.toLowerCase())){
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+
+    // find the current month name and add it to the month list
+    public String findCurrentMonth(){
+        return LocalDate.now().getMonth().toString().toLowerCase().substring(0, 3);
+    }
 }
