ad1f212b1ecea4b08a58444e073a00a9483c7670
Aliherawi7 <Aliherawi7@gmail.com>
2022-10-30 09:47:48 +0430

    comment repository added with its query methods

--- src/main/java/com/eshop/repository/CommentRepository.java
+++ src/main/java/com/eshop/repository/CommentRepository.java
@@ -1,4 +1,12 @@
 package com.eshop.repository;
 
-public interface CommentRepository {
+import com.eshop.model.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Collection;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    Collection<Comment> findAllByProductId(Long productId);
+    Collection<Comment> findAllByUserId(Long userId);
+
 }
