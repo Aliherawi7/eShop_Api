d90ecebb4d2f5606d7f5d3ddb1bd3a7d43512045
Aliherawi7 <Aliherawi7@gmail.com>
2022-10-30 09:47:00 +0430

    comment properties and behaviors added

--- src/main/java/com/eshop/model/Comment.java
+++ src/main/java/com/eshop/model/Comment.java
@@ -1,4 +1,79 @@
 package com.eshop.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
 public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private Long productId;
+    private Long userId;
+    private LocalDateTime commentDate;
+    private String comment;
+    private int rate;
+
+    public Comment() {
+    }
+
+    public Comment(Long id, Long productId, Long userId, LocalDateTime commentDate, String comment, int rate) {
+        this.id = id;
+        this.productId = productId;
+        this.userId = userId;
+        this.commentDate = commentDate;
+        this.comment = comment;
+        this.rate = rate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public LocalDateTime getCommentDate() {
+        return commentDate;
+    }
+
+    public void setCommentDate(LocalDateTime commentDate) {
+        this.commentDate = commentDate;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public int getRete() {
+        return rate;
+    }
+
+    public void setRete(int rate) {
+        this.rate = rate;
+    }
 }
