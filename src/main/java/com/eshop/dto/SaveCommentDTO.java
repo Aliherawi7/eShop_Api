60fcdcb0de1b35f15de9be12e9d1231165d96203
Aliherawi7 <Aliherawi7@gmail.com>
2022-10-30 16:02:10 +0430

    SaveCommentDTO created to get the comment info from the client side to save on the database. properties and methods added

--- src/main/java/com/eshop/dto/SaveCommentDTO.java
+++ src/main/java/com/eshop/dto/SaveCommentDTO.java
@@ -1,4 +1,50 @@
 package com.eshop.dto;
 
 public class SaveCommentDTO {
+    private long id;
+    private long productId;
+    private String comment;
+    private int rate;
+
+    public SaveCommentDTO() {
+    }
+
+    public SaveCommentDTO(long id, long productId, String comment, int rate) {
+        this.id = id;
+        this.productId = productId;
+        this.comment = comment;
+        this.rate = rate;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(long productId) {
+        this.productId = productId;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public int getRate() {
+        return rate;
+    }
+
+    public void setRate(int rate) {
+        this.rate = rate;
+    }
 }
