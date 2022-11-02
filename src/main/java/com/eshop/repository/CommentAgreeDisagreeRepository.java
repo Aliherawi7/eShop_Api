6d2fe7b2345873989c0a1e778e54ae8d607e9841
Aliherawi7 <Aliherawi7@gmail.com>
2022-11-02 22:48:02 +0430

    query methods added

--- src/main/java/com/eshop/repository/CommentAgreeDisagreeRepository.java
+++ src/main/java/com/eshop/repository/CommentAgreeDisagreeRepository.java
@@ -1,4 +1,13 @@
 package com.eshop.repository;
 
-public interface CommentAgreeDisagreeRepository {
+import com.eshop.model.AgreeDisagree;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Collection;
+
+public interface CommentAgreeDisagreeRepository extends JpaRepository<AgreeDisagree, Long> {
+    Collection<AgreeDisagree> findAllByCommentIdAndUserId(long commentId, long userId);
+    Collection<AgreeDisagree> findAllByCommentIdAndAgree(long commentId, boolean agree);
+    Collection<AgreeDisagree> findAllByCommentIdAndDisagree(long commentId, boolean disagree);
+    AgreeDisagree findByCommentIdAndUserId(long commentId, long userId);
 }
