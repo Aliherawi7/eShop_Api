c91fbc381eb8143f90c7eeacbfa3cf8731b3d4bc
ali herawi <aliherawi7@gmail.com>
2023-03-30 22:19:59 +0430

    user profile picture and product image lcations added

--- src/main/java/com/eshop/properties/FileStorageLocation.java
+++ src/main/java/com/eshop/properties/FileStorageLocation.java
@@ -1,4 +1,34 @@
 package com.eshop.properties;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
 public class FileStorageLocation {
+
+    private String userProfileUploadDir;
+    private String productUploadDir;
+
+    public FileStorageLocation(
+            @Value("${file.user.profile.upload.dir}") String userProfileUploadDir,
+            @Value("${file.product.upload.dir}") String productUploadDir) {
+        this.userProfileUploadDir = userProfileUploadDir;
+        this.productUploadDir = productUploadDir;
+    }
+
+    public String getUserProfileUploadDir() {
+        return userProfileUploadDir;
+    }
+
+    public void setUserProfileUploadDir(String userProfileUploadDir) {
+        this.userProfileUploadDir = userProfileUploadDir;
+    }
+
+    public String getProductUploadDir() {
+        return productUploadDir;
+    }
+
+    public void setProductUploadDir(String productUploadDir) {
+        this.productUploadDir = productUploadDir;
+    }
 }
