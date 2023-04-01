88977ea1849792d54c2cc3c375f84cc1de62c88b
ali herawi <aliherawi7@gmail.com>
2023-04-01 14:22:32 +0430

    productDTOMapper added and com.eshop.repository.ProductImageRepository removed

--- src/main/java/com/eshop/service/FavoriteProductService.java
+++ src/main/java/com/eshop/service/FavoriteProductService.java
@@ -3,9 +3,7 @@ package com.eshop.service;
 import com.eshop.dto.ProductDTO;
 import com.eshop.model.FavoriteProduct;
 import com.eshop.model.Product;
-import com.eshop.model.ProductSidesImages;
 import com.eshop.repository.FavoriteProductRepository;
-import com.eshop.repository.ProductImageRepository;
 import com.eshop.repository.ProductRepository;
 import org.springframework.stereotype.Service;
 
@@ -16,40 +14,29 @@ import java.util.Collection;
 public class FavoriteProductService {
      private final FavoriteProductRepository favoriteProductRepository;
      private final ProductRepository productRepository;
-     private final ProductImageRepository productImageRepository;
+     private final ProductDTOMapper productDTOMapper;
 
-    public FavoriteProductService(FavoriteProductRepository favoriteProductRepository, ProductRepository productRepository, ProductImageRepository productImageRepository) {
+    public FavoriteProductService(FavoriteProductRepository favoriteProductRepository,
+                                  ProductRepository productRepository,
+                                  ProductDTOMapper productDTOMapper) {
         this.favoriteProductRepository = favoriteProductRepository;
         this.productRepository = productRepository;
-        this.productImageRepository = productImageRepository;
+        this.productDTOMapper = productDTOMapper;
     }
 
     public Collection<ProductDTO> getFavoriteProducts(String email){
-        System.out.println(email);
        Collection<FavoriteProduct> favoriteProducts = favoriteProductRepository.findAllByUserEmail(email);
-        System.out.println(favoriteProducts);
        Collection<ProductDTO> products = new ArrayList<>();
 
        // dou to Null pointer exception
        if(favoriteProducts.size() > 0){
            favoriteProducts.forEach(favoriteProduct -> {
               Product product = productRepository.findById(favoriteProduct.getProductId()).get();
-              ProductDTO productDTO = new ProductDTO();
-              productDTO.setProductId(product.getId());
-              productDTO.setPrice(product.getPrice());
-              productDTO.setCategory(product.getCategory());
-              productDTO.setDiscount(product.getDiscount());
-              productDTO.setName(product.getName());
-              productDTO.setRate(product.getRate());
-              ProductSidesImages productSidesImages =
-                       productImageRepository.findByProductId(product.getId());
-              ArrayList<byte[]> image = new ArrayList<>();
-              image.add(productSidesImages.getSide1());
-              productDTO.setImages(image);
+              ProductDTO productDTO = productDTOMapper.apply(product);
               products.add(productDTO);
            });
-           return products;
-       }else return products;
+       }
+        return products;
     }
 
     public boolean addFavoriteProduct(FavoriteProduct favoriteProduct){
