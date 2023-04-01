303862163e30b296bcf6ba392921f1bf28c53eaa
ali herawi <aliherawi7@gmail.com>
2023-04-01 14:25:26 +0430

    productDTOMapper added

--- src/main/java/com/eshop/service/ProductService.java
+++ src/main/java/com/eshop/service/ProductService.java
@@ -1,10 +1,9 @@
 package com.eshop.service;
 
 import com.eshop.dto.ProductDTO;
+import com.eshop.exception.ProductNotFoundException;
 import com.eshop.model.Product;
-import com.eshop.model.ProductSidesImages;
 import com.eshop.repository.CommentRepository;
-import com.eshop.repository.ProductImageRepository;
 import com.eshop.repository.ProductRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,49 +12,41 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.time.LocalDate;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 public class ProductService {
     private final ProductRepository productRepository;
-    private final ProductImageRepository productImageRepository;
     private final CommentRepository commentRepository;
+    private final ProductDTOMapper productDTOMapper;
+    private final FileStorageService fileStorageService;
 
-    public ProductService(ProductRepository productRepository, ProductImageRepository productImageRepository, CommentRepository commentRepository) {
+    public ProductService(ProductRepository productRepository,
+                          CommentRepository commentRepository,
+                          ProductDTOMapper productDTOMapper,
+                          FileStorageService fileStorageService) {
         this.productRepository = productRepository;
-        this.productImageRepository = productImageRepository;
         this.commentRepository = commentRepository;
+        this.productDTOMapper = productDTOMapper;
+        this.fileStorageService = fileStorageService;
     }
 
     //find product by id
     public ProductDTO getProductById(Long id) {
 
         Optional<Product> optionalProduct = productRepository.findById(id);
-        ProductDTO productDTO = new ProductDTO();
         if (optionalProduct.isPresent()) {
+            ProductDTO productDTO ;
             Product product = optionalProduct.get();
-            productDTO.setName(product.getName());
-            productDTO.setProductId(product.getId());
-            productDTO.setName(product.getName());
-            productDTO.setBrandName(product.getBrandName());
-            productDTO.setCategory(product.getCategory());
-            productDTO.setColors(new String[]{product.getColor()});
-            productDTO.setDiscount(product.getDiscount());
-            productDTO.setPrice(product.getPrice());
-            productDTO.setRate(product.getRate());
-            productDTO.setProductionDate(product.getProductionDate());
-            productDTO.setSize(product.getSize());
-            productDTO.setQuantityInDepot(product.getQuantityInDepot());
+            productDTO = productDTOMapper.apply(product);
             productDTO.setReviews(commentRepository.findAllByProductId(product.getId()).size());
-            ArrayList<String> description = new ArrayList<>(Arrays.asList(product.getDescription().split("\n")));
-            productDTO.setDetails(description);
-            ProductSidesImages productSidesImages =
-                    productImageRepository.findByProductId(product.getId());
-            productDTO.setImages(new ArrayList<>(Arrays.asList(productSidesImages.getSide1(), productSidesImages.getSide2(), productSidesImages.getSide3())));
-
+            return productDTO;
         }
-        return productDTO;
+        throw new ProductNotFoundException("product not found with provided id: "+id);
     }
 
     //find product in the database by name
@@ -64,8 +55,8 @@ public class ProductService {
     }
 
     // save the product to the database
-    public ResponseEntity<?> addProduct(Product product) {
-        return new ResponseEntity<>(productRepository.save(product), HttpStatus.CREATED);
+    public void addProduct(Product product) {
+        new ResponseEntity<>(productRepository.save(product), HttpStatus.CREATED);
     }
     // save the product with images to the database
     public ProductDTO addProduct(ArrayList<MultipartFile> files, Map<String, String> params) {
@@ -80,19 +71,14 @@ public class ProductService {
         product.setQuantityInDepot(Long.parseLong(params.get("quantityInDepot")));
         product.setSize(params.get("size"));
         product.setDiscount(Double.parseDouble(params.get("discount")));
-        ProductSidesImages productSidesImages = new ProductSidesImages();
         try {
-            productSidesImages.setProductId(product.getId());
-            productSidesImages.setSide1(files.get(0).getBytes());
-            productSidesImages.setSide2(files.get(1).getBytes());
-            productSidesImages.setSide3(files.get(2).getBytes());
-            productImageRepository.save(productSidesImages);
+            fileStorageService.storeProductImage(files.get(0), product.getId()+"side-1");
+            fileStorageService.storeProductImage(files.get(1), product.getId()+"side-2");
+            fileStorageService.storeProductImage(files.get(2), product.getId()+"side-3");
         } catch (IOException e) {
             e.printStackTrace();
         }
         long productId = productRepository.save(product).getId();
-        productSidesImages.setProductId(productId);
-        productImageRepository.save(productSidesImages);
         return getProductById(productId);
     }
 
@@ -179,9 +165,8 @@ public class ProductService {
         productRepository.deleteById(id);
         if (isExist) {
             return new ResponseEntity<>("product with id: " + id + " successfully removed", HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>("not found any product with id: " + id, HttpStatus.NO_CONTENT);
         }
+        throw new ProductNotFoundException("not found any product with id: " + id);
 
     }
 
@@ -192,28 +177,8 @@ public class ProductService {
     }
 
     public ProductDTO getProductDTO(Product product){
-        ProductDTO productDTO = new ProductDTO();
-        productDTO.setName(product.getName());
-        productDTO.setProductId(product.getId());
-        productDTO.setName(product.getName());
-        productDTO.setBrandName(product.getBrandName());
-        productDTO.setCategory(product.getCategory());
-        productDTO.setColors(new String[]{product.getColor()});
-        productDTO.setDiscount(product.getDiscount());
-        productDTO.setPrice(product.getPrice());
-        productDTO.setRate(product.getRate());
-        productDTO.setProductionDate(product.getProductionDate());
-        productDTO.setSize(product.getSize());
-        productDTO.setQuantityInDepot(product.getQuantityInDepot());
+        ProductDTO productDTO = productDTOMapper.apply(product);
         productDTO.setReviews(commentRepository.findAllByProductId(product.getId()).size());
-        ArrayList<String> description = new ArrayList<>(Arrays.asList(product.getDescription().split("\n")));
-        productDTO.setDetails(description);
-
-        ProductSidesImages productSidesImages =
-                productImageRepository.findByProductId(product.getId());
-        ArrayList<byte[]> image = new ArrayList<>();
-        image.add(productSidesImages.getSide1());
-        productDTO.setImages(image);
         return productDTO;
     }
 
