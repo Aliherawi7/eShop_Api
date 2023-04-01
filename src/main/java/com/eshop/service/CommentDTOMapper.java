e2e87d5088d4d82bfd9ece917eb53f5b5a9d6d4d
ali herawi <aliherawi7@gmail.com>
2023-04-01 14:20:47 +0430

    this component is used to convert Comment into CommentDTO

--- src/main/java/com/eshop/service/CommentDTOMapper.java
+++ src/main/java/com/eshop/service/CommentDTOMapper.java
@@ -1,4 +1,24 @@
 package com.eshop.service;
 
-public class CommentDTOMapper {
+import com.eshop.dto.CommentDTO;
+import com.eshop.model.Comment;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Function;
+
+@Component
+public class CommentDTOMapper implements Function<Comment, CommentDTO> {
+    @Override
+    public CommentDTO apply(Comment comment) {
+        return new CommentDTO(
+                comment.getId(),
+                null,
+                comment.getMessage(),
+                comment.getRate(),
+                comment.getCommentDate(),
+                null,
+                0,
+                0
+        );
+    }
 }
