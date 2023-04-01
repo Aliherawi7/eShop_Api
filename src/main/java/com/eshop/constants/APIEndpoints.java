76f78063d41d3a857155b45546356231ae2482e0
ali herawi <aliherawi7@gmail.com>
2023-04-01 14:13:57 +0430

    common endpoint added

--- src/main/java/com/eshop/constants/APIEndpoints.java
+++ src/main/java/com/eshop/constants/APIEndpoints.java
@@ -1,4 +1,15 @@
 package com.eshop.constants;
 
 public enum APIEndpoints {
+    USER_PICTURE("api/v1/files/user-profile/"),
+    PRODUCT_IMAGES("api/v1/files/product-image/");
+
+    final String value;
+    APIEndpoints(String value){
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
