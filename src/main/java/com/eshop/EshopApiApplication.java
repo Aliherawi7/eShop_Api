edc2a310a099fea09f062faa355b63ebe4b3f024
ali herawi <aliherawi7@gmail.com>
2023-04-02 13:12:30 +0430

    command line runner update4d

--- src/main/java/com/eshop/EshopApiApplication.java
+++ src/main/java/com/eshop/EshopApiApplication.java
@@ -9,14 +9,18 @@ import com.eshop.service.BrandService;
 import com.eshop.service.OrderService;
 import com.eshop.service.ProductService;
 import com.eshop.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.net.URL;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 
 @SpringBootApplication
@@ -33,7 +37,7 @@ public class EshopApiApplication {
 
 
     @Bean
-    CommandLineRunner run(ProductService p, UserService userService,
+    CommandLineRunner run(ProductService productService, UserService userService,
                           RoleRepository roleRepository, OrderService orderService,
                           BrandService brandService,
                           CommentRepository commentRepository,
@@ -49,11 +53,9 @@ public class EshopApiApplication {
                     "Alexajhonson@gmail.com", LocalDate.of(2000, 3, 25),
                     "12345",null , "USA");
 
-
             UserSignupDTO user3 = new UserSignupDTO("Amanda", "Jepson",
                     "Amandajepson@gmail.com", LocalDate.of(2000, 3, 25),
                     "12345",null , "UK");
-
 
             Role role_user = new Role(1, "USER");
             Role role_admin = new Role(2, "ADMIN");
@@ -65,172 +67,19 @@ public class EshopApiApplication {
             userService.addRoleToUser(user1.getEmail(), role_admin.getName());
 
             // static products
-
-
-
-
-
-
-
-
-
-
-            Product p1 = new Product(
-                    1L, "iPhone-14-Pro-Max-Purple",
-                    "#ebebe7",
-                    "apple", "mobile",
-                    1000d, "latest iphone", LocalDate.now(), "medium",120L, 4d, 14D
-            );
-
-            Product p2 = new Product(
-                    2L, "Samsung TV", "#171717",
-                    "Samsung", "TV", 500d, "family tv",
-                    LocalDate.now(), "medium",168L, 3d, 10D
-            );
-
-            Product p3 = new Product(
-                    3L, "macbook pro", "#c0c1c4",
-                    "apple", "pc", 960d, "laptop pc",
-                    LocalDate.now(), "medium",650L, 5d, 10D
-            );
-
-            Product p4 = new Product(
-                    4L, "iphone 14 pro space dark", "#c81e30",
-                    "apple", "mobile", 1560d, "latest iphone",
-                    LocalDate.now(), "pro",1500L, 5d, 6D
-            );
-
-            Product p5 = new Product(
-                    5L, "MacBook 14 pro", "#c0c1c6",
-                    "apple", "pc", 1000d, "latest mac",
-                    LocalDate.now(), "mini",1000L, 4d, 5D
-            );
-
-
-            Product p6 = new Product(
-                    6L, "asus-gaming", "#121212",
-                    "asus", "pc", 30d, "latest asus-gaming",
-                    LocalDate.now(), "large",623L, 5d, 10D
-            );
-
-            Product p7 = new Product(
-                    7L, "Full HD LED TV", "#eaeaea",
-                    "Sharp", "TV", 30d, "latest Sharp",
-                    LocalDate.now(), "large",23L, 4d, 0D
-            );
-
-            Product p8 = new Product(
-                    8L, "Apple watch 14 pro", "#dcdcdc",
-                    "apple", "tools", 300d, "latest apple watch",
-                    LocalDate.now(), "medium",730L, 3d, 10D
-            );
-
-            Product p9 = new Product(
-                    9L, "modern stylish watch", "#f7f6f2",
-                    "apple", "tools", 99d, "latest apple watch",
-                    LocalDate.now(), "mini",200L, 5d, 8D
-            );
-
-            Product p10 = new Product(
-                    10L, "Samsung A71", "#94e3d0",
-                    "samsung", "mobile", 500d, "latest samsung phone",
-                    LocalDate.now(), "large",1600L, 4d, 5D
-            );
-
-            Product p11 = new Product(
-                    11L, "Modern Headphone", "#4f659f",
-                    "jax", "tools", 500d, "latest jax headphone",
-                    LocalDate.now(), "large",900L, 5d, 10D
-            );
-
-            Product p12 = new Product(
-                    12L, "Headphone 600x", "#c23533",
-                    "jax", "tools", 500d, "latest jax headphone",
-                    LocalDate.now(), "large",1180L, 4d, 8D
-            );
-
-            Product p13 = new Product(
-                    13L, "airpods", "#ebecf0",
-                    "apple", "tools", 500d, "latest airpod",
-                    LocalDate.now(), "large",1700L, 3d, 10D
-            );
-
-            Product p14 = new Product(
-                    14L, "iphone 13 pro max silver", "#05c7c9",
-                    "apple", "mobile", 500d, "iphone 13 pro max silver 128gb",
-                    LocalDate.now(), "pro max",1400L, 5d, 15D
-            );
-
-            Product p15 = new Product(
-                    15L, "H10", "#3cbdbb",
-                    "Xiaomi", "mobile", 500d, "latest Xiaomi ",
-                    LocalDate.now(), "large",1490L, 3d, 10D
-            );
-
-            Product p16 = new Product(
-                    16L, "Laptop c06", "#525252",
-                    "HP", "pc", 500d, "latest HP ",
-                    LocalDate.now(), "large",120L, 5d, 5D
-            );
-
-            Product p17 = new Product(
-                    17L, "ipad pro 14 ", "#edeeec",
-                    "HP", "pc", 500d, "latest HP ",
-                    LocalDate.now(), "large",100L, 4d, 10D
-            );
-
-            Product p18 = new Product(
-                    18L, "ASUS studio Book", "#494753",
-                    "Asus", "pc", 500d, "latest Asus ",
-                    LocalDate.now(), "large",70L, 4d, 8D
-            );
-
-            Product p19 = new Product(
-                    19L, "acer studio Book", "#aeafb3",
-                    "acer", "pc", 500d, "latest acer ",
-                    LocalDate.now(), "large",320L, 4d, 10D
-            );
-
-            Product p20 = new Product(
-                    20L, "philips-takh 402", "#59c3fd",
-                    "philips", "tools", 500d, "latest philips headphone ",
-                    LocalDate.now(), "large",50L, 4d, 5D
-            );
-
-            //change the updated time
-            p6.setUpdateInDepot(LocalDate.of(2022, 1, 23));
-            p15.setUpdateInDepot(LocalDate.of(2022, 2, 23));
-            p14.setUpdateInDepot(LocalDate.of(2022, 3, 23));
-            p13.setUpdateInDepot(LocalDate.of(2022, 4, 23));
-            p12.setUpdateInDepot(LocalDate.of(2022, 5, 23));
-            p11.setUpdateInDepot(LocalDate.of(2022, 6, 23));
-            p10.setUpdateInDepot(LocalDate.of(2022, 7, 23));
-            p8.setUpdateInDepot(LocalDate.of(2022, 8, 23));
-            p4.setUpdateInDepot(LocalDate.of(2022, 9, 23));
-            p3.setUpdateInDepot(LocalDate.of(2022, 10, 23));
-            p16.setUpdateInDepot(LocalDate.of(2021, 11, 23));
-            p5.setUpdateInDepot(LocalDate.of(2021, 12, 23));
-
-            p.addProduct(p1);
-            p.addProduct(p2);
-            p.addProduct(p3);
-            p.addProduct(p4);
-            p.addProduct(p5);
-            p.addProduct(p6);
-            p.addProduct(p7);
-            p.addProduct(p8);
-            p.addProduct(p9);
-            p.addProduct(p10);
-            p.addProduct(p11);
-            p.addProduct(p12);
-            p.addProduct(p13);
-            p.addProduct(p14);
-            p.addProduct(p15);
-            p.addProduct(p16);
-            p.addProduct(p17);
-            p.addProduct(p18);
-            p.addProduct(p19);
-            p.addProduct(p20);
+            ObjectMapper objectMapper = new ObjectMapper();
+            ArrayList<Product> products = new ArrayList<>();
+            try {
+                URL jsonUrl = Thread.currentThread().getContextClassLoader().getResource("static\\products.json");
+                Product[] array = objectMapper.readValue(jsonUrl, Product[].class);
+                products.addAll(Arrays.asList(array));
+                products.forEach(item -> {
+                    System.out.println(item.getName());
+                    productService.addProduct(item);
+                });
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
 
             // static orders
 
@@ -254,35 +103,35 @@ public class EshopApiApplication {
             orderService.addOrder(order6);
             // static brand
 
-            Brand b1 = new Brand(null, "dell",null);
-            Brand b2 = new Brand(null, "hp",null);
-            Brand b4 = new Brand(null, "microsoft",null);
-            Brand b5 = new Brand(null, "samsung",null);
-            Brand b6 = new Brand(null, "huawei",null);
-            Brand b7 = new Brand(null, "apple",null);
-
+            Brand b2 = new Brand(2, "dell",null);
+            Brand b3 = new Brand(3, "hp",null);
+            Brand b5 = new Brand(5, "microsoft",null);
+            Brand b6 = new Brand(6, "samsung",null);
+            Brand b4 = new Brand(4, "huawei",null);
+            Brand b1 = new Brand(1, "apple",null);
             brandService.addBrand(b1);
             brandService.addBrand(b2);
+            brandService.addBrand(b3);
             brandService.addBrand(b4);
             brandService.addBrand(b5);
             brandService.addBrand(b6);
-            brandService.addBrand(b7);
+
 
 
             // static comments
 
-            p.getAllProducts().forEach(item ->{
-                Comment c2 = new Comment(null, item.getProductId(), 2, LocalDateTime.now(),
-                        "oh so expensive!", 5);
-                Comment c3 = new Comment(null, item.getProductId(), 3, LocalDateTime.now(),
-                        "is there any role for changing the arrived product?", 4);
-                commentRepository.save(c2);
-                commentRepository.save(c3);
-            });
+//            productService.getAllProducts().forEach(item ->{
+//                Comment c2 = new Comment(null, item.getProductId(), 2, LocalDateTime.now(),
+//                        "oh so expensive!", 5);
+//                Comment c3 = new Comment(null, item.getProductId(), 3, LocalDateTime.now(),
+//                        "is there any role for changing the arrived product?", 4);
+//                commentRepository.save(c2);
+//                commentRepository.save(c3);
+//            });
 
-//            AgreeDisagree like1 =
-//                    new AgreeDisagree(null, true, false, LocalDateTime.now(),2L, 2L);
-//            commentAgreeDisagreeRepository.save(like1);
+            AgreeDisagree like1 =
+                    new AgreeDisagree(null, true, false, LocalDateTime.now(),2L, 2L);
+            commentAgreeDisagreeRepository.save(like1);
 
         };
     }
