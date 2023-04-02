b23f94e28f6746abaae9538415b8ab196b4bf42f
ali herawi <aliherawi7@gmail.com>
2023-04-02 13:14:38 +0430

    brand-images endpoint added

--- src/main/java/com/eshop/constants/APIEndpoints.java
+++ src/main/java/com/eshop/constants/APIEndpoints.java
@@ -2,7 +2,8 @@ package com.eshop.constants;
 
 public enum APIEndpoints {
     USER_PICTURE("api/v1/files/user-profile/"),
-    PRODUCT_IMAGES("api/v1/files/product-image/");
+    PRODUCT_IMAGES("api/v1/files/product-image/"),
+    BRAND_IMAGES("api/v1/files/brand-image/");
 
     final String value;
     APIEndpoints(String value){
