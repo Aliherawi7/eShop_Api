a302cbb07a6a92bb5a04a2bdf0e7174dd728b930
ali herawi <aliherawi7@gmail.com>
2023-04-05 17:16:55 +0430

    all required proparties added

--- src/main/java/com/eshop/dto/ProductRegistrationRequest.java
+++ src/main/java/com/eshop/dto/ProductRegistrationRequest.java
@@ -2,7 +2,7 @@ package com.eshop.dto;
 
 import org.springframework.web.multipart.MultipartFile;
 
-public class ProductRegisterationRequest {
+public class ProductRegistrationRequest {
     private String name;
     private String color;
     private MultipartFile imageSide1;
@@ -17,22 +17,22 @@ public class ProductRegisterationRequest {
     private double rate;
     private double discount;
 
-    public ProductRegisterationRequest() {
+    public ProductRegistrationRequest() {
     }
 
-    public ProductRegisterationRequest(String name,
-                                       String color,
-                                       MultipartFile imageSide1,
-                                       MultipartFile imageSide2,
-                                       MultipartFile imageSide3,
-                                       String brandName,
-                                       String category,
-                                       double price,
-                                       String descriptions,
-                                       String size,
-                                       long quantityInDepot,
-                                       double rate,
-                                       double discount) {
+    public ProductRegistrationRequest(String name,
+                                      String color,
+                                      MultipartFile imageSide1,
+                                      MultipartFile imageSide2,
+                                      MultipartFile imageSide3,
+                                      String brandName,
+                                      String category,
+                                      double price,
+                                      String descriptions,
+                                      String size,
+                                      long quantityInDepot,
+                                      double rate,
+                                      double discount) {
         this.name = name;
         this.color = color;
         this.imageSide1 = imageSide1;
@@ -46,6 +46,109 @@ public class ProductRegisterationRequest {
         this.quantityInDepot = quantityInDepot;
         this.rate = rate;
         this.discount = discount;
-        this.reviews = reviews;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public MultipartFile getImageSide1() {
+        return imageSide1;
+    }
+
+    public void setImageSide1(MultipartFile imageSide1) {
+        this.imageSide1 = imageSide1;
+    }
+
+    public MultipartFile getImageSide2() {
+        return imageSide2;
+    }
+
+    public void setImageSide2(MultipartFile imageSide2) {
+        this.imageSide2 = imageSide2;
+    }
+
+    public MultipartFile getImageSide3() {
+        return imageSide3;
+    }
+
+    public void setImageSide3(MultipartFile imageSide3) {
+        this.imageSide3 = imageSide3;
+    }
+
+    public String getBrandName() {
+        return brandName;
+    }
+
+    public void setBrandName(String brandName) {
+        this.brandName = brandName;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescriptions() {
+        return descriptions;
+    }
+
+    public void setDescriptions(String descriptions) {
+        this.descriptions = descriptions;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public long getQuantityInDepot() {
+        return quantityInDepot;
+    }
+
+    public void setQuantityInDepot(long quantityInDepot) {
+        this.quantityInDepot = quantityInDepot;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
     }
 }
