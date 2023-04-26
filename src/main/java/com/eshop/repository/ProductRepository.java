2872e27f22c8f80ef1d21f437efbe99a540e2e64
ali herawi <aliherawi7@gmail.com>
2023-04-26 18:08:16 +0430

    query methods updated with fetching data useing containing concept

--- src/main/java/com/eshop/repository/ProductRepository.java
+++ src/main/java/com/eshop/repository/ProductRepository.java
@@ -5,28 +5,35 @@ import com.eshop.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Collection;
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     // find all product by brand name
     Collection<Product> findAllByBrandName(String brandName);
 
     // find all product by category
-    Collection<Product> findAllByCategory(String category);
+    Collection<Product> findAllByKeywordsContaining(String category);
 
     // find product bt name
     Product findByName(String name);
+
+    // find product by name keyword
+    List<Product> findAllByNameContaining(String keyword);
+
+    // find product by name keyword
+    List<Product> findAllByNameContainingOrKeywordsContainingOrBrandNameContaining(String name, String category, String brand);
 
     // check if product exist by name
     boolean existsProductByName(String name);
 
     //check if product exist by brand name
-    boolean existsProductByBrandName(String name);
+    boolean existsProductByBrandNameContaining(String name);
 
     // find all product by category and brand name and price greater than given price
-    Collection<Product> findAllByCategoryAndBrandNameAndPriceGreaterThan(String category, String brandName, Double price);
+    Collection<Product> findAllByKeywordsContainingAndBrandNameAndPriceGreaterThan(String category, String brandName, Double price);
 
     // find all product by category and brand name and price less than given price
-    Collection<Product> findAllByCategoryAndBrandNameAndPriceLessThan(String category, String brandName, Double price);
+    Collection<Product> findAllByKeywordsContainingAndBrandNameAndPriceLessThan(String category, String brandName, Double price);
 
     // find all product by price greater or equal to the given price
     Collection<Product> findAllByPriceGreaterThanEqual(Double price);
@@ -35,16 +42,16 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     Collection<Product> findAllByPriceLessThanEqual(Double price);
 
     // find all product by category and price less than or equal to the given price
-    Collection<Product> findAllByCategoryAndPriceLessThanEqual(String category, Double price);
+    Collection<Product> findAllByKeywordsContainingAndPriceLessThanEqual(String category, Double price);
 
     // find all product by category and price greater or equal to the given price
-    Collection<Product> findAllByCategoryAndPriceGreaterThanEqual(String category, Double price);
+    Collection<Product> findAllByKeywordsContainingAndPriceGreaterThanEqual(String category, Double price);
 
     // find all product by brand name and price less than or equal to the given price
-    Collection<Product> findAllByBrandNameAndPriceLessThanEqual(String brandName, Double price);
+    Collection<Product> findAllByBrandNameContainingAndPriceLessThanEqual(String brandName, Double price);
 
     // find all product by brand name and price greater than or equal to the given price
-    Collection<Product> findAllByBrandNameAndPriceGreaterThanEqual(String brandName, Double price);
+    Collection<Product> findAllByBrandNameContainingAndPriceGreaterThanEqual(String brandName, Double price);
 
 
 }
