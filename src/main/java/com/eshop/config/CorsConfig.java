7e7050d38f459f89dbbb99e8d04bc6f1e7bca608
ali herawi <aliherawi7@gmail.com>
2023-04-29 07:10:09 +0430

    cors configs updated

--- src/main/java/com/eshop/config/CorsConfig.java
+++ src/main/java/com/eshop/config/CorsConfig.java
@@ -21,25 +21,12 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOriginPatterns(allowedOrigin)
+                registry.addMapping("/*")
+                        .allowedOrigins(allowedOrigin)
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*");
             }
         };
-    }
-
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.getAllowCredentials();
-        config.addAllowedOrigin("http://localhost:3000");
-        config.addAllowedHeader("*");
-        config.setAllowedOrigins(Arrays.asList(allowedOrigin, "http://localhost:3000/*"));
-        config.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE", "PUT"));
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
     }
 
 }
